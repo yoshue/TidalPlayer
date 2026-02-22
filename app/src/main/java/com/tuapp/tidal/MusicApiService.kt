a5ba200a4af5a9c76defa9bae985093aa1c157e7
yoshue <52592749+yoshue@users.noreply.github.com>
2026-02-22 05:11:22 -0300

    Update MusicApiService.kt

--- app/src/main/java/com/tuapp/tidal/MusicApiService.kt
+++ app/src/main/java/com/tuapp/tidal/MusicApiService.kt
@@ -4,8 +4,7 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface MusicApiService {
-    // Cambiamos la ruta de "api/proxy/search" a solo "search" o la ruta directa
-    @GET("search") 
+    @GET("search")
     suspend fun searchTracks(
         @Query("query") query: String,
         @Query("type") type: String = "TRACKS"
