0ab11e7924ef5b62be6a1dc39a21f720871fb098
yoshue <52592749+yoshue@users.noreply.github.com>
2026-02-22 04:02:04 -0300

    Update TrackModels.kt

--- app/src/main/java/com/tuapp/tidal/TrackModels.kt
+++ app/src/main/java/com/tuapp/tidal/TrackModels.kt
@@ -1,15 +1,5 @@
 package com.tuapp.tidal
 
-data class SearchResponse(
-    val items: List<Track>
-)
-
-data class Track(
-    val id: String,
-    val title: String,
-    val artist: Artist,
-    val album: Album
-)
-
+data class SearchResponse(val items: List<Track>)
+data class Track(val id: String, val title: String, val artist: Artist)
 data class Artist(val name: String)
-data class Album(val cover: String)
